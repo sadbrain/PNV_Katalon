4e65e34276bcf7180bacdf2436152cf6909c9f8c
Thanh Van <van.tran25@student.passerellesnumeriques.org>
2025-01-03 14:00:32 +0700

    Write test case script of TC011_VerifyColorAndFont

--- Scripts/TC011_VerifyColorAndFont/Script1735886813756.groovy
+++ Scripts/TC011_VerifyColorAndFont/Script1735886813756.groovy
@@ -17,3 +17,52 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
+def rgbaToHex(String rgba) {
+	if (rgba.contains("rgba")) {
+		def rgbValues = rgba.replace("rgba(", "").replace(")", "").split(",")
+		String red = rgbValues[0].trim()
+		String green = rgbValues[1].trim()
+		String blue = rgbValues[2].trim()
+		
+		return String.format("#%02X%02X%02X", red.toInteger(), green.toInteger(), blue.toInteger())
+	} else if (rgba.contains("rgb")) {
+		def rgbValues = rgba.replace("rgb(", "").replace(")", "").split(",")
+		String red = rgbValues[0].trim()
+		String green = rgbValues[1].trim()
+		String blue = rgbValues[2].trim()
+		
+		return String.format("#%02X%02X%02X", red.toInteger(), green.toInteger(), blue.toInteger())
+	} else {
+		throw new IllegalArgumentException("Invalid color format: " + rgba)
+	}
+}
+
+'Pre-condition steps: '
+'Open the browser and navigate to the URL'
+WebUI.openBrowser(GlobalVariable.URL_HEROKU)
+
+'Step 1: '
+'Navigate to Challenging DOM page'
+WebUI.click(findTestObject('Object Repository/Heroku_Page/lnk_Challenging_Dom'))
+
+'Verify that "Challenging DOM" header title is displayed'
+WebUI.verifyTextPresent("Challenging DOM", false)
+
+'Step 2: '
+'Verify the font size of Button 1 is 16px'
+WebUI.verifyMatch(WebUI.getCSSValue(findTestObject('Object Repository/Challenging_Dom_Page/btn_1'), 'font-size'), '16px', false)
+
+'Step 3: '
+'Verify background-color of Button 2 is #c60f13'
+String buttonColor = WebUI.getCSSValue(findTestObject('Object Repository/Challenging_Dom_Page/btn_2'), 'background-color')
+String hexColor = rgbaToHex(buttonColor)
+WebUI.verifyMatch(hexColor, '#C60F13', false)
+
+'Step 4: '
+'Verify border-color of Button 3 is #457a1a'
+String buttonBorderColor = WebUI.getCSSValue(findTestObject('Object Repository/Challenging_Dom_Page/btn_3'), 'border-color')
+String hexBorderColor = rgbaToHex(buttonBorderColor)
+WebUI.verifyMatch(hexBorderColor, '#457A1A', false)
+
+'Clean-up steps:'
+WebUI.closeBrowser()
