0f52e52e34c9cb37cca7c2177378aa4dd91e1a39
Mixchennie0505 <nhung.phan@sapotacorp.vn>
2025-01-03 13:00:12 +0700

    Write test case script of TC008_VerifyFrames

---
+++ Object Repository/Heroku_Page/lnk_WYSIWYGEditor.rs
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>lnk_WYSIWYGEditor</name>
+   <tag></tag>
+   <elementGuidId>f0754057-a6e8-4e14-aba1-305d7e6b0c0b</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//a[normalize-space()='WYSIWYG Editor']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
---
+++ Object Repository/TinyMCE_Page/h3_Title.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>h3_Title</name>
+   <tag></tag>
+   <elementGuidId>04351fce-3bd5-4a05-b595-dc128a3bc36a</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//h3[normalize-space()='An iFrame containing the TinyMCE WYSIWYG Editor']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
