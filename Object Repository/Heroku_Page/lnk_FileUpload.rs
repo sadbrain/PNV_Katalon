33190fb098185997b4cf981fc325faa05b59d285
Mixchennie0505 <nhung.phan@sapotacorp.vn>
2025-01-03 09:32:09 +0700

    Write test case script of TC007_VerifyFileUpload

---
+++ Object Repository/Heroku_Page/lnk_FileUpload.rs
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>lnk_FileUpload</name>
+   <tag></tag>
+   <elementGuidId>8ab9b1a3-3073-494a-9594-24d8ab0ccac8</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//a[normalize-space()='File Upload']</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
