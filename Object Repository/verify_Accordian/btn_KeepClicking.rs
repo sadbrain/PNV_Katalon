f5928487514e09759ad856b8ca9296f210c9e271
rinaksor <146155132+rinaksor@users.noreply.github.com>
2025-01-03 09:55:41 +0700

    TC003 - Verify Accordian

---
+++ Object Repository/verify_Accordian/btn_KeepClicking.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>btn_KeepClicking</name>
+   <tag></tag>
+   <elementGuidId>00e4bbaf-d1c5-46b0-8d75-d1b065f8012c</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//*[@id=&quot;click-accordion&quot;]</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
---
+++ Object Repository/verify_Accordian/btn_Loading.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>btn_Loading</name>
+   <tag></tag>
+   <elementGuidId>b5b4d14d-bde8-4651-8aef-64c7e4af3ed3</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//*[@id=&quot;hidden-text&quot;]</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
