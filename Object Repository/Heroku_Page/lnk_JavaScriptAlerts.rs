ebd4a2f85db254002ad6467b4bf043d47e3932c9
Mixchennie0505 <nhung.phan@sapotacorp.vn>
2025-01-03 13:08:07 +0700

    Write test case script of TC009_VerifyAlerts

---
+++ Object Repository/Heroku_Page/lnk_JavaScriptAlerts.rs
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>lnk_JavaScriptAlerts</name>
+   <tag></tag>
+   <elementGuidId>b4f4dee5-feab-4fcd-8ccd-a4512610144e</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//a[normalize-space()='JavaScript Alerts']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
