29fcd59827a1417784e8212cc088c86207b39248
Thanh Van <van.tran25@student.passerellesnumeriques.org>
2025-01-03 13:53:51 +0700

    Write test case script of TC010_VerifyKeyPresses

---
+++ Object Repository/Key_Presses_Page/h_KeyPressesTitle.rs
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>h_KeyPressesTitle</name>
+   <tag></tag>
+   <elementGuidId>b74e78ce-0c59-4acc-a5e9-c4ba6e857bbb</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id=&quot;content&quot;]//h3[text()=&quot;Key Presses&quot;]</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
---
+++ Scripts/TC010_VerifyKeyPresses/Script1735886576167.groovy
@@ -0,0 +1,68 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+'Pre-condition steps: '
+'Open the browser and navigate to the URL'
+WebUI.openBrowser(GlobalVariable.URL_HEROKU)
+
+'Step 1: '
+'Navigate to press key page'
+WebUI.click(findTestObject('Object Repository/Heroku_Page/lnk_KeyPresses'))
+'Verify Key Presses title is diplayed'
+WebUI.verifyElementText(findTestObject('Object Repository/Key_Presses_Page/h_KeyPressesTitle'), 'Key Presses')
+
+'Step 2: '
+'Press TAB key'
+WebUI.sendKeys(findTestObject('Object Repository/Key_Presses_Page/frm_KeyPressesForm'), Keys.chord(Keys.TAB))
+'Verify the Messagse "You entered: TAB" is displayed'
+WebUI.verifyElementText(findTestObject('Object Repository/Key_Presses_Page/p_KeyPressResult'), 'You entered: TAB')
+
+'Step 3: '
+'Press ENTER key'
+WebUI.sendKeys(findTestObject('Object Repository/Key_Presses_Page/frm_KeyPressesForm'), Keys.chord(Keys.ENTER))
+'Verify the Messagse "You entered: ENTER" is displayed'
+WebUI.verifyElementText(findTestObject('Object Repository/Key_Presses_Page/p_KeyPressResult'), 'You entered: ENTER')
+
+'Step 4: '
+'Press G key'
+WebUI.sendKeys(findTestObject('Object Repository/Key_Presses_Page/frm_KeyPressesForm'), Keys.chord("G"))
+'Verify the Messagse "You entered: G" is displayed'
+WebUI.verifyElementText(findTestObject('Object Repository/Key_Presses_Page/p_KeyPressResult'), 'You entered: G')
+
+'Clean-up steps:'
+WebUI.closeBrowser()
