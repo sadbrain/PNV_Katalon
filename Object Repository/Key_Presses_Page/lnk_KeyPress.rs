892d4ede695b23fa42c6192a0e957e15de764b3b
bichquyenle <lethibichquyen5804@gmail.com>
2025-01-03 19:16:48 +0700

    Finish TC010- Verify Key Presses

---
+++ Object Repository/Key_Presses_Page/lnk_KeyPress.rs
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>lnk_KeyPress</name>
+   <tag></tag>
+   <elementGuidId>9d82057a-14b9-45a8-9983-a04463e2fa5d</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//a[normalize-space()='Key Presses']</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
