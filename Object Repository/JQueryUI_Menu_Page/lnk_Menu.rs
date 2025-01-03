d51ff19babfef54c1b278b8fec9f45e11b5dc37a
Thanh Van <van.tran25@student.passerellesnumeriques.org>
2025-01-03 14:11:48 +0700

    Write test case script of TC013_VerifyMenu

---
+++ Object Repository/JQueryUI_Menu_Page/lnk_Menu.rs
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>lnk_Menu</name>
+   <tag></tag>
+   <elementGuidId>6f59b271-be87-41e6-b470-76e57592b0b8</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//a[normalize-space()='Menu']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
